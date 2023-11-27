8e1e7b7bf2618a32adc8cf676402a376efe017d7
Gerónimo Basso <geronimobasso@Geronimos-MacBook-Pro.local>
2023-11-27 08:54:43 +0100

    Added assets

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sierra Donaciones a ex militares</title>
+</head>
+<body>
+    <div>
+        hola
+    </div>
+</body>
+</html>
