058b2543340bf754d48d026dc28324612faa4569
geronimobasso <gerobasso01@gmail.com>
2023-11-27 21:02:25 +0100

    Added inclues folder, and more assets

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="flexbox-container-header">
+        <div class="flexbox-container-logo">
+            <img src="assets/images/logo-header.svg">
+            <ul>Sierra</ul>
+        </div>
+        <nav>
+            <ul>Inicio</ul>
+            <ul>Donación</ul>
+            <ul>Contacto</ul>
+            <ul>Sobre nosotros</ul>
+            <ul>Iniciar Sesión</ul>
+            <ul>Registrarse</ul>
+        </nav>
+    </div>
+</header>
