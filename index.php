e03ec93114e7e3759d10b26a419901dadf06daa3
geronimobasso <gerobasso01@gmail.com>
2023-11-27 21:01:28 +0100

    Added basic structure to the index.php

--- index.php
+++ index.php
@@ -3,11 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sierra Donaciones a ex militares</title>
+    <title>Sierra | Donaciones a ex militares</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/logo-header.svg">
+    <link rel="sylesheet" href="assets/style/style.css">
 </head>
 <body>
+<?php include 'includes/header.php'; ?>
+<main>
     <div>
-        hola
+        <h1>En Sierra recaudamos donaciones para ex militares españoles.</h1>
+        <button>Haz una donación</button>
+        <div>
+            <img src="assets/images/army-soldier-walking.svg">
+            <img src="assets/images/army-soldier-walking.svg">
+            <img src="assets/images/army-soldier-walking.svg">
+        </div>
     </div>
+</main>
+<aside class="grid-logos-army">
+    <img src="assets/images/army-logo-1.svg">
+    <img src="assets/images/army-logo-2.svg">
+    <img src="assets/images/army-logo-3.svg">
+    <img src="assets/images/army-logo-4.svg">
+</aside>
+<?php include 'includes/footer.php'; ?>
 </body>
 </html>
