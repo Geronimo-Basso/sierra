80e0c4f9050df19574b395edfbc14c38b22ab543
geronimobasso <gerobasso01@gmail.com>
2023-12-10 18:42:33 +0100

    Made about-us.php

--- about-us.php
+++ about-us.php
@@ -1,1 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require 'head.php'?>
+</head>
+<body>
+<?php require 'includes/header.php'; ?>
+<div class="about-us-main-content">
+    <div class="about-us-main-content-home">
+        <h1>Sobre nosotros</h1>
+        <img style ="height: " alt="flying-spanish-cazas" src="assets/images/flying-cazas.png">
+    </div>
+    <aside class="about-us-aside">
+        <p>
+            Bienvenido a Sierra, una fundación dedicada a brindar apoyo vital a ex militares y sus familias.<br><br>
+            En Sierra, entendemos que el compromiso y sacrificio de nuestros héroes no termina cuando dejan el servicio militar; es el inicio de una nueva batalla, una en la que estamos comprometidos a estar a su lado.<br><br>
+            Nuestra Misión:<br><br>
+            En Sierra, nos enorgullece ofrecer ayuda sólido para aquellos que han servido valientemente.<br><br>
+            Nos esforzamos por proporcionar un apoyo integral, abordando las diversas necesidades de los ex militares y sus familias a medida que transitan por las transiciones de la vida civil.<br><br>
+            Desde la vivienda de apoyo hasta programas de salud y compensación financiera, estamos comprometidos a ser un faro de esperanza y ayuda.<br><br>
+            ¡Gracias por ser parte de esta noble causa!
+        </p>
+
+    </aside>
+</div>
+<?php require 'includes/footer.php'; ?>
+</body>
+</html>
