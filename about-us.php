00a0ccb7f2d3d61bfc73836100ca7d55bc8e691f
geronimobasso <gerobasso01@gmail.com>
2023-12-11 09:01:16 +0100

    Added about us

--- about-us.php
+++ about-us.php
@@ -2,26 +2,24 @@
 <html lang="en">
 <head>
     <?php require 'head.php'?>
+    <title>Hola</title>
 </head>
 <body>
 <?php require 'includes/header.php'; ?>
 <div class="about-us-main-content">
-    <div class="about-us-main-content-home">
-        <h1>Sobre nosotros</h1>
-        <img style ="height: " alt="flying-spanish-cazas" src="assets/images/flying-cazas.png">
-    </div>
-    <aside class="about-us-aside">
-        <p>
-            Bienvenido a Sierra, una fundación dedicada a brindar apoyo vital a ex militares y sus familias.<br><br>
-            En Sierra, entendemos que el compromiso y sacrificio de nuestros héroes no termina cuando dejan el servicio militar; es el inicio de una nueva batalla, una en la que estamos comprometidos a estar a su lado.<br><br>
-            Nuestra Misión:<br><br>
-            En Sierra, nos enorgullece ofrecer ayuda sólido para aquellos que han servido valientemente.<br><br>
-            Nos esforzamos por proporcionar un apoyo integral, abordando las diversas necesidades de los ex militares y sus familias a medida que transitan por las transiciones de la vida civil.<br><br>
-            Desde la vivienda de apoyo hasta programas de salud y compensación financiera, estamos comprometidos a ser un faro de esperanza y ayuda.<br><br>
-            ¡Gracias por ser parte de esta noble causa!
-        </p>
+    <h1>Sobre nosotros</h1>
+    <p class="about-us-main-text">Bienvenido a Sierra, una fundación que no solo se dedica, sino que se compromete con pasión y profundo respeto a brindar apoyo vital a nuestros ex militares y sus familias. En Sierra, no solo reconocemos la valentía y el servicio incondicional de nuestros héroes militares, sino que también entendemos profundamente que su compromiso y sacrificio no se detiene al dejar el uniforme. Para muchos, dejar el servicio militar marca el inicio de una nueva batalla, una batalla llena de desafíos únicos y a menudo abrumadores. En esta nueva fase de sus vidas, Sierra está aquí, firme y constante, para estar a su lado y apoyarlos en cada paso del camino. <br>
 
-    </aside>
+        Nuestra Misión en Sierra se centra en ser una columna de fortaleza y estabilidad. Nos enorgullece ofrecer un apoyo sólido y constante para aquellos que han servido valientemente a nuestra nación. Este apoyo no es solo una promesa, sino una realidad tangible que se manifiesta en cada aspecto de nuestro trabajo. Nos esforzamos incansablemente por proporcionar un apoyo integral, cubriendo no solo las necesidades básicas, sino también abordando las diversas y complejas necesidades de los ex militares y sus familias a medida que navegan por las complicadas transiciones de la vida civil.<br>
+
+        En Sierra, entendemos que cada veterano y cada familia tienen su propia historia única y sus desafíos individuales. Por ello, ofrecemos una amplia gama de servicios diseñados para abordar cada aspecto de estas transiciones. Desde la vivienda de apoyo, que ofrece un lugar seguro y estable para comenzar una nueva vida, hasta programas completos de salud mental y física, entendemos que el bienestar de nuestros veteranos es multifacético. También estamos profundamente comprometidos con la provisión de asistencia para la compensación financiera, la educación y el empleo, reconociendo que la estabilidad económica es fundamental para el éxito a largo plazo en la vida civil.<br>
+
+        Pero nuestro trabajo no se detiene ahí. En Sierra, también abogamos incansablemente por los derechos y el reconocimiento de nuestros veteranos, trabajando para asegurar que sus voces sean escuchadas y sus necesidades reconocidas a nivel nacional. Nos esforzamos por crear un cambio positivo y duradero, no solo para los individuos a los que servimos directamente, sino para la comunidad de veteranos en general.<br>
+
+        Ser parte de Sierra significa ser parte de una familia extendida; una comunidad que se extiende más allá del servicio militar, donde cada miembro es valorado, respetado y apoyado. Nos unimos en la misión común de honrar y cuidar a aquellos que han dado tanto por nosotros.<br>
+
+        ¡Gracias por ser parte de esta noble causa! Su apoyo nos ayuda a continuar nuestro trabajo esencial, brindando esperanza, ayuda y una nueva oportunidad para aquellos que han servido a nuestra nación con honor.
+    </p>
 </div>
 <?php require 'includes/footer.php'; ?>
 </body>
--- head.php
+++ head.php
@@ -4,10 +4,6 @@
 <meta property="og:title" content="Bienvenido a Sierra - Apoyo a Ex Militares y sus Familias">
 <meta property="og:description" content="Sierra: Fundación dedicada al apoyo de ex militares y sus familias. Ofrecemos ayuda en vivienda, salud y más. Descubre nuestra misión y cómo puedes contribuir.">
 <meta property="og:type" content="website">
-<meta property="og:url" content="[Your Website URL]">
-<meta property="og:image" content="[URL of an image representing the page]">
-<link rel="alternate" hreflang="es" href="[Your Spanish Page URL]">
-<link rel="canonical" href="[The Preferred URL of This Page]">
 <link rel="icon" type="image/x-icon" href="assets/images/logo-header.svg">
 <link rel="stylesheet" href="assets/style/style.css">
 <title>Bienvenido a Sierra - Apoyo a Ex Militares y sus Familias</title>
