9db028e99e4a0ed19ae82246ae7d96d4171e2654
geronimobasso <gerobasso01@gmail.com>
2023-12-11 10:04:47 +0100

    Added register template

--- sign-up.php
+++ sign-up.php
@@ -1,4 +1,26 @@
-<?php require 'includes/header.php'; ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require 'head.php'?>
+</head>
+<body>
+<div class="login-in-flexbox-main-content">
+    <div class="login-in-flexbox-item-1">
+        <img style='height: 270px' src="assets/images/logo-header.svg">
+        <h1>Te damos la bienvenida</h1>
+    </div>
+    <div class="login-in-flexbox-item-2">
+        <h2>Inicia sesión en Sierra</h2>
+        <p>¿No tiene cuenta? <a> <u>Registrate</u> </a></p>
+        <form class="login-in-form">
+            <input type="text" id="email" name="email" placeholder="Email"><br>
+            <input type="password" id="password" name="password" placeholder="Contraseña"><br>
+            <p><a><u>¿Olvidaste tu contraseña?</u> </a></p>
+            <input type="submit" value="Iniciar sesión">
+            <p>Al iniciar sesión aceptas <a> <u>terminos y condiciones.</u> </a></p>
+        </form>
+    </div>
+</div>
 <?php require 'includes/footer.php'; ?>
-
+</body>
+</html>
