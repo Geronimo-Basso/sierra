a839c119687e3acee5729e977f4f49a2413361cf
geronimobasso <gerobasso01@gmail.com>
2024-01-02 13:13:04 +0100

    Made user have a table instead of p

--- user.php
+++ user.php
@@ -1,7 +1,13 @@
 <?php
+session_start();
+
+if (!$_SESSION['donor_email']) {
+    header("Location: " . "index.php");
+    exit();
+}
+
 /** @var mysqli $connection */
 require_once 'helper.php';
-session_start();
 //Get user info
 $user_email = $_SESSION['donor_email'];
 $user = user_information($user_email, $connection);
@@ -15,6 +21,7 @@ $user = user_information($user_email, $connection);
     <?php require 'includes/header.php'; ?>
     <?php require 'includes/headernavmobile.php'; ?>
     <main class="user-content">
+        <h1>Información personal:</h1>
         <form class="user-form" method="post">
             <label>Email</label>
             <input type="text" value="<?php echo htmlspecialchars($user['email']); ?>" name="email" readonly>
@@ -22,16 +29,29 @@ $user = user_information($user_email, $connection);
             <input type="text" value="<?php echo htmlspecialchars($user['name']);?>" name="name" readonly>
             <label>Apellidos</label>
             <input type="text" value="<?php echo htmlspecialchars($user['lastname']); ?>" name="lastname" readonly>
-            <label>Donaciones realizadas:</label>
-            <?php
-                $donations = donation_by_user($user_email,$connection);
+            <p>Donaciones realizadas:<br></p>
+            <table>
+                <thead>
+                <tr>
+                    <th>Titulo de campaña</th>
+                    <th>Cantidad donada</th>
+                    <th>Fecha donación</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $donations = donation_by_user($user_email, $connection);
                 foreach ($donations as $donation):
-                    $campaign = campaign_by_id($donation['id_campaign'],$connection);
-                ?>
-                    <p><?php echo htmlspecialchars($campaign['title']); ?></p>
-                    <p><?php echo htmlspecialchars($donation['amount']); ?></p>
-                    <p><?php echo htmlspecialchars($donation['datetime']); ?></p>
-                <?php endforeach;?>
+                    $campaign = campaign_by_id($donation['id_campaign'], $connection);
+                    ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($campaign['title']); ?></td>
+                        <td><?php echo htmlspecialchars($donation['amount']) . '€' ; ?></td>
+                        <td><?php echo htmlspecialchars($donation['datetime']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
         </form>
         <form action="includes/close-session.php" method="post">
             <input type="submit" value="Cerrar Sesión">
