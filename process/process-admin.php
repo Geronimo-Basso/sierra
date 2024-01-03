1c1c906cf31b28b871dd1f7e0213aeb59d8728ca
geronimobasso <gerobasso01@gmail.com>
2024-01-03 12:27:15 +0100

    Upload image is working

--- process/process-admin.php
+++ process/process-admin.php
@@ -9,9 +9,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $target = $_POST['target'];
     $image = $_FILES['image'];
 
+    // Define the target directory and file
+    $targetDir = __DIR__ . '/../uploads/'; // Directory where files will be saved
+    $targetFile = $targetDir . basename($image["name"]); // Path of the file to be uploaded
+
+    // Move the uploaded file to the target path
+    if (move_uploaded_file($image["tmp_name"], $targetFile)) {
+        echo "The file has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+
     $saved_campaing = save_campaign($title,$description,$target,$image,$connection);
 
-    if ($saved_campaing){
+    if ($saved_campaing) {
         $_SESSION['saved'] = 'Campaña guardada con exito';
     } else {
         $_SESSION['error'] = 'ERROR, la campaña no se guardo';
@@ -19,4 +30,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header("Location: ../admin.php");
     exit();
 }
+
 mysqli_close($connection);
